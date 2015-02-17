9dce51ddd42e5ddd247a7425cf5f0943783ec070
Peter Fiorella <Peter@peterfiorella.com>
2015-02-17 11:51:59 -0700

    Added comments and cleaned up edit and user pages

--- edit.php
+++ edit.php
@@ -1,49 +1,46 @@
 <?php 
 require_once "php/utils.php";
 
-$id = $_GET['id'];
+/*User and edit page require the use of id query strings to differentiate users. 
+e.g. user.php?id=1 -> User 1
+	 user.php?id=2 -> User 2
+*/
+
+$id = $_GET['id']; //get id query string from URL
 
 if(isset($_POST["Edit"])){
-		$summaryText = htmlspecialchars($_POST['summary']);
-		$interestText = htmlspecialchars($_POST['interests']);
-		writeToFile($id."summary.txt", $summaryText);
-		writeToFile($id."interests.txt", $interestText);
-		header("Location: user.php?id=$id");
+		$summaryText = htmlspecialchars($_POST['summary']); //sanitize summary text
+		$interestText = htmlspecialchars($_POST['interests']); // santize interests text
+		writeToFile($id."summary.txt", $summaryText); //write summary to file (id + summary (e.g. 1summary.txt for User 1))
+		writeToFile($id."interests.txt", $interestText); //write interests to file
+		header("Location: user.php?id=$id"); //redirect back to user page, with proper ID query string
 			
 	}	
-
 	include "header.php"; 
-
-	
-
 ?>
-
 <div id="picture">
 	<div class="profilePic">
 	<img  src="assets/<?php echo $id?>.jpg" alt="User's profile picture" />
 	</div>
 </div>
 <div id="content">
-<div id="inner">
-<?php 
-
-	if($id == 1){ 
-		echo "<h2 class='name'>User 1</h2>";
-	}else if ($id == 2){ 
-		echo "<h2 class='name'>User 2</h2>";
-	}else{
-
-	}
+	<div id="inner">
+	<?php
+		if($id == 1){ 
+			echo "<h2 class='name'>User 1</h2>";
+		}else if ($id == 2){ 
+			echo "<h2 class='name'>User 2</h2>";
+		}else{}
 	?>
-<form method="post">
-<h3>Summary</h3>
-<textarea class="txt" name="summary" rows='4' cols='50'><?php readFromFile($id.'summary.txt')?></textarea>
-<h3>Interests</h3>
-<textarea class="txt" name="interests" rows='4' cols='50'><?php readFromFile($id.'interests.txt')?></textarea><br />
-<div class="btn">
-<input type="submit" class="button" name="Edit" value="Done Editing" />
-</div>		
-</form>
-</div>
+		<form method="post">
+			<h3>Summary</h3>
+				<textarea class="txt" name="summary" rows='4' cols='50'><?php readFromFile($id.'summary.txt')?></textarea>
+			<h3>Interests</h3>
+				<textarea class="txt" name="interests" rows='4' cols='50'><?php readFromFile($id.'interests.txt')?></textarea><br />
+			<div class="btn">
+				<input type="submit" class="button" name="Edit" value="Done Editing" />
+			</div>		
+		</form>
+	</div>
 </div>
 <?php include "footer.php"?>
--- user.php
+++ user.php
@@ -1,42 +1,41 @@
 <?php 
 require_once "php/utils.php";
-$id = $_GET['id'];
-if(isset($_POST["Edit"])){
-	if(canEdit()){
-			header("Location: edit.php?id=$id");
+/*User and edit page require the use of id query strings to differentiate users. 
+e.g. user.php?id=1 -> User 1
+	 user.php?id=2 -> User 2
+*/
+$id = $_GET['id']; //get id query string from URL
+if(isset($_POST["Edit"])){ //if edit button has been clicked
+	if(canEdit()){ //check if user has edit permission
+			header("Location: edit.php?id=$id"); //redirect to edit page
 			return;
 	}
 }	
 include "header.php"; 
 ?>
-
-
-
 <div id="picture">
 	<div class="pPic">
 	<img width="70%" src="assets/<?php echo $id?>.jpg" alt="User's profile picture" />
 	</div>
 </div>
 <div id="content">
-<div id="inner">
-
-<h3>Summary</h3>
-<p><?php  readFromFile($id . 'summary.txt')?></p>
-<h3>Interests</h3>
-<p><?php  readFromFile($id . 'interests.txt')?></p>
-<form method="post">
-<div class="btn">
-<input type="hidden" name="User" value="<?php echo $id?>">
-<input  type="submit" class="button" name="Edit" value="Edit" />
-</div>
-<?php 
-if(isset($_POST["Edit"])){
-	if(!canEdit()){ 
-	echo "<h4 style='color: red;'>Sorry, you are not allowed to edit.</h4>";
-	}
-}
-?>		
-</form>
-</div>
+	<div id="inner">
+		<h3>Summary</h3>
+		<p><?php  readFromFile($id . 'summary.txt')?></p>
+		<h3>Interests</h3>
+		<p><?php  readFromFile($id . 'interests.txt')?></p>
+		<form method="post">
+		<div class="btn">
+			<input  type="submit" class="button" name="Edit" value="Edit" />
+		</div>
+		<?php 
+		if(isset($_POST["Edit"])){
+			if(!canEdit()){ //if user cannot edit, display error message
+				echo "<h4 style='color: red;'>Sorry, you are not allowed to edit.</h4>";
+			}
+		}
+		?>		
+		</form>
+	</div>
 </div>
 <?php include "footer.php"?>
