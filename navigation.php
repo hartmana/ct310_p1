81a157aa309e0699ae6b5cdf956f02bc8b23dc8f
hartmana <aaron@hrtmn.net>
2015-02-18 16:09:28 -0700

    Basic structure and div's in place.
    div ID's:
        header
        wrapper
        footer
    
    div classes:
        content

--- navigation.php
+++ navigation.php
@@ -2,12 +2,12 @@
 
 echo <<< NAV
 
-        <div id="nav">
-            <ul>
-                <li><a href="" alt="Homepage">Home</a></li>
-                <li><a href="" alt="Search page">Search</a></li>
-            </ul>
-        </div>
+            <div id="nav">
+                <ul>
+                    <li><a href="" alt="Homepage">Home</a></li>
+                    <li><a href="" alt="Search page">Search</a></li>
+                </ul>
+            </div>
 
 NAV;
 
